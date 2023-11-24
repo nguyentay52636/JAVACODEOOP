b29837a94c28bd7ab01beb0bae3b9fc4594a0cc5
huybang2017 <ndhbang2017@gmail.com>
2023-11-25 02:29:45 +0700

    fixing

--- src/MainCore/Memu.java
+++ src/MainCore/Memu.java
@@ -1,5 +1,28 @@
 package MainCore;
 
 public class Memu {
-    
+    private String name;
+    private int Price;
+
+    public Memu(String name, int price) {
+        this.name = name;
+        Price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPrice() {
+        return Price;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPrice(int price) {
+        Price = price;
+    }
+
 }
